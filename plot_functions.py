6e0a12212ea3adf5b3ee29a1c30d6912e1fe28e0
Popp <jo28dohe@fauad.fau.de>
2021-09-29 14:52:14 +0200

    test

--- plot_functions.py
+++ plot_functions.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import pandas as pd
 
-### TODO: typical figure sizes 
+###  TODO: typical figure sizes 
 ##- twocolumn paper: 3.375,3.375*3/4  (inches) or 8.57 cm , 8.57*3/4 cm
 
 class paperfigure:
